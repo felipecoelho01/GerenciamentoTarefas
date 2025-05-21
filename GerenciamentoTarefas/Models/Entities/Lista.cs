3225016a184a60762644f89d0e3e5c42c580467b
felipecoelho01 <felipecoelho12@outlook.com.br>
2025-05-21 12:58:45 -0300

    Adicionar arquivos de projeto.

---
+++ GerenciamentoTarefas/Models/Entities/Lista.cs
@@ -0,0 +1,9 @@
+﻿namespace GerenciamentoTarefas.Models.Entities
+{
+    public class Lista
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+    }
+}
---
+++ GerenciamentoTarefas/Services/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using GerenciamentoTarefas.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GerenciamentoTarefas.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Lista> TbLogin { get; set; }
+    }
+}
